0280032e4bacd0a1537d9ef756c7950aab349773
Ivan Rodriguez <frivanrodriguez@gmail.com>
2015-10-14 00:23:20 +0200

    Add writing new element

--- elements/index.js
+++ elements/index.js
@@ -1,7 +1,36 @@
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
-  destination: function () {
-    console.log('method 1 just ran', this.templatePath());
-  }
+
+  prompting: function () {
+    var done = this.async();
+    this.prompt({
+      type    : 'input',
+      name    : 'name',
+      message : 'Your element name',
+      default : 'new-element' // Default to current folder name
+    }, function (answers) {
+      this.elementName = answers.name;
+      done();
+    }.bind(this));
+  },
+
+  writing: function () {
+
+    var fileTypes = ['.js', '.html', '.css'];
+
+    for (var fileType in fileTypes) {
+      this.fs.copyTpl(
+        this.templatePath('my-element' + fileTypes[fileType]),
+        this.destinationPath('elements/my-' + this.elementName + fileTypes[fileType]),
+        { elementName: this.elementName }
+      );
+    }
+  },
+
+  // destination: function () {
+  //   console.log('method 1 just ran', this.templatePath());
+  //   console.log('answer', this.answer);
+  // }
+
 });
---
+++ elements/templates/my-element.js
@@ -0,0 +1,4 @@
+
+Polymer({
+  is: 'my-<%= elementName %>'
+});
